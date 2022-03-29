80bfe3b4de51968fe2bd7235c46ddc551b2eb2f0
Pila <teyim.pila@gmail.com>
2022-03-30 01:57:08 +0200

    hide footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./modules/yelpSearch/containers/HomePage";
 import Header from "./shared/Header";
-import Footer from "./shared/Footer";
 
 function App() {
   return (
@@ -10,9 +9,8 @@ function App() {
       <Header/>
       <Routes>
         <Route path={'/'} element={<HomePage/>}/>
-        {/*  TODO: Add 404 page */}
+        {/* TODO: Add 404 page */}
       </Routes>
-      <Footer/>
     </div>
   );
 }
